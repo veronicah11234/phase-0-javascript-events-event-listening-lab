daf028d7da0a539a1140c8724878a0cd257a5e3f
miriam <miriamwangui525@gmail.com>
2024-01-03 15:26:46 +0300

    using the event listeners

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
 function addingEventListener() {
-}
+    const input = document.getElementById('input');
+  
+    function clickAlert() {
+      alert('I was clicked!');
+    }
+  
+    input.addEventListener('click', clickAlert);
+  }
+  
+  const sinon = require('sinon');
+  let input;
+  
+  beforeEach(function () {
+    input = document.getElementById('input');
+    sinon.spy(input, 'addEventListener');
+  })
+  
+  it("binds an event listener in addingEventListener()", () => {
+    addingEventListener();
+    expect(input.addEventListener.called).to.be.true;
+  })
+  
